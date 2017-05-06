ea05b87f1f8a94e70eb27e1cd503a09f08e40868
renjie1996 <2578370399@qq.com>
2017-05-06 22:41:54 +0800

    Add files via upload

--- 小程序KFC/pages/menu/menu.js
+++ 小程序KFC/pages/menu/menu.js
@@ -1,26 +1,30 @@
-let items = ['item1', 'item2', 'item3', 'item4']
+// 初始化动画实例
+let animation = wx.createAnimation({
+  duration: 400,
+  timingFunction: "linear",
+  delay: 0
+});
 Page({
   data: {
     scollTop: 0,
-    scroll_into_view: 'foodtype0',
-    listArray: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
-    imgArray: [],
-    foodArray: [],
-    shoppingList: [],
-    totalPrice: 0,
-    totalCount: 0,
-    movelength: 0,
-    cartIsHidden: true,
-    cartIndexIsHidden: true,
-    actionSheetHidden: true,
-    actionSheetItems: items,
-    animationData: {}
+    scroll_into_view: 'foodtype0', // scroll-view的初始位置
+    imgArray: [], // 导航的图片数组，通过ajax获取
+    foodArray: [], // 食品的对象数组
+    shoppingList: [], // 购物车储存的对象数组
+    totalPrice: 0,  // 购物车的总价格
+    totalCount: 0,  // 购物车的总数量
+    movelength: 0,  // 上移或下拉动画的单位距离
+    cartIsHidden: true, // 购物车是否隐藏
+    cartIndexIsHidden: true, // 购物车详情菜单是否隐藏
+    animationData: {} // 动画动作对象
   },
+  // 设置标题名
   onReady: function () {
     wx.setNavigationBarTitle({
       title: '菜单'
     })
   },
+  // 加载ajax的最佳时机
   onLoad: function (options) {
     let that = this
     wx.showToast({
@@ -29,6 +33,7 @@ Page({
       duration: 0,
       mask: true
     })
+    // 发送请求
     wx.request({
       url: 'http://easy-mock.com/mock/5905d4597a878d73716e2c6b/kfc/kfc',
       method: 'GET',
@@ -46,26 +51,17 @@ Page({
     }, function () {
       wx.hideToast();
     })
-    // setTimeout(function() {
-    //  wx.hideToast();
-    // },2000)
-
   },
-  click: function (e) {
-    console.log(e)
-  },
+  // 跳页的id
   changepage: function (e) {
-    var goPage = e.currentTarget.id
-    console.log(goPage)
+    // 滚动到指定的id
+    let goPage = e.currentTarget.id
     this.setData({
       scroll_into_view: "foodtype" + goPage
     })
   },
+  // 点击‘+’添加进购物车
   addShopcart: function (e) {
-
-    // console.log(e.target.dataset.foodidx)
-    // console.log(e.target.dataset.price)
-    // console.log(e.target.dataset.name)
     let move_length = this.data.movelength;
     let shopping_list = this.data.shoppingList;
     let total_price = this.data.totalPrice;
@@ -74,19 +70,23 @@ Page({
     let itemNum = 1;
     let that = this;
 
-    // 同种判断
+    // 是否有同种商品判断
     if (this.data.shoppingList.length > 0) {
+      // 商品名是否相同判断，不重复添加同名商品
       let isHave = this.data.shoppingList.findIndex(item => item.name == e.target.dataset.name)
       if (isHave != -1) {
         that.data.shoppingList[isHave].num++
       } else {
+        // 购物车数组加进新的一样食品
         that.data.shoppingList.push({
           price: e.target.dataset.price,
           name: e.target.dataset.name,
           num: itemNum
         })
+        // 动画效果的长度添加
         move_length++
       }
+    // 没有商品时直接添加
     } else {
       this.data.shoppingList.push({
         price: e.target.dataset.price,
@@ -95,18 +95,11 @@ Page({
       })
       move_length++
     }
-    console.log(this.data.shoppingList)
-    // shopping_list.push({
-    //       price: e.target.dataset.price,
-    //       name: e.target.dataset.name,
-    //       num: itemNum
-    //     })
-
-    let animation = wx.createAnimation({
-      duration: 400,
-      timingFunction: "linear",
-      delay: 0
-    });
+    // 动画上拉长度对应的bottom的计算
+    /**
+     *  mlength是bottom的长度
+     *  animation.bottom(mlength).step() 加入动画队列
+     */
     let mlength = move_length * 55;
     if (move_length > 1) {
       mlength = 55 + (move_length - 1) * 65;
@@ -114,23 +107,19 @@ Page({
     this.animation = animation
     animation.bottom(mlength).step()
     this.setData({
-      // movelength: move_length,
       animationData: animation.export()
     })
     this.setData({
       shoppingList: shopping_list,
       totalPrice: total_price,
       totalCount: total_count,
+      // 购物车当有商品时弹出
       cartIsHidden: false,
       movelength: move_length
     })
   },
+  // 购物车详情抽屉点击时弹出
   showCart: function (e) {
-    let animation = wx.createAnimation({
-      duration: 400,
-      timingFunction: "linear",
-      delay: 0
-    });
     let move_length = 0;
     move_length = this.data.movelength * 55;
     if (this.data.movelength > 1) {
@@ -140,11 +129,11 @@ Page({
     animation.bottom(move_length).step()
     let cart_indexIsHidden = !this.data.cartIndexIsHidden;
     this.setData({
-      // movelength: move_length,
       cartIndexIsHidden: cart_indexIsHidden,
       animationData: animation.export()
     })
   },
+  // 购物车详情抽屉中增加数量
   addShopcartInCart: function (e) {
     let total_count = this.data.totalCount + 1;
     let addTarget = this.data.shoppingList.findIndex(item => item.name === e.target.dataset.name);
@@ -156,21 +145,20 @@ Page({
       totalCount: total_count
     })
   },
+  // 购物车详情抽屉中减少数量,但没有商品时需要抽屉下降并购物车组件消失
   deleteShopcartInCart: function (e) {
     let move_length = this.data.movelength;
+     // 选定被点击的元素
     let addTarget = this.data.shoppingList.findIndex(item => item.name === e.target.dataset.name);
     let tempPrice = 0;
     let total_count = this.data.totalCount - 1;
     this.data.shoppingList[addTarget].num--;
     if (this.data.shoppingList[addTarget].num < 1) {
+      // 总价的减少
       tempPrice = parseInt(this.data.totalPrice) - parseInt(this.data.shoppingList[addTarget].price)
       this.data.shoppingList.splice(addTarget, 1);
       move_length--;
-      let animation = wx.createAnimation({
-        duration: 400,
-        timingFunction: "linear",
-        delay: 0
-      });
+      // bottom值变化产生动画
       let mlength = move_length * 61;
       if (move_length < 2) {
         mlength = move_length * 50
@@ -181,9 +169,11 @@ Page({
       console.log(this.data.totalPrice)
       this.animation = animation
       animation.bottom(mlength).step()
+      // 总数等于0时购物车组件设置为消失
       if (total_count == 0) {
         let cart_isHidden = !this.data.cartIsHidden
         console.log(cart_isHidden)
+        // 这里设置一个计时器，让下拉抽屉动画完成后再消失组件，不然体验性太差
         setTimeout(() => {
           this.setData({
             cartIsHidden: cart_isHidden
@@ -198,10 +188,10 @@ Page({
         totalPrice: tempPrice
       })
     } else {
+      // 计算出来新的价格
       tempPrice = parseInt(this.data.totalPrice) - parseInt(this.data.shoppingList[addTarget].price)
       console.log(this.data.totalPrice)
     }
-
     this.setData({
       shoppingList: this.data.shoppingList,
       totalPrice: tempPrice,
@@ -210,11 +200,13 @@ Page({
   },
   toCount: function (e) {
     console.log(this.data.totalCount)
+    // 菜单对象
     let OrderMenu = {
       list: this.data.shoppingList,
       price: this.data.totalPrice,
       count: this.data.totalCount
     }
+    // 本地存储菜单对象
     wx.setStorage({
       key: "OrderMenu",
       data: OrderMenu
